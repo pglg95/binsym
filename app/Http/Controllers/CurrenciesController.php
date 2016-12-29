4e3266efa47b5ab9393d71254c55f67e13bdddf1
pglg <pglg95@op.pl>
2016-12-29 17:17:07 +0100

    added html parser

--- app/Http/Controllers/CurrenciesController.php
+++ app/Http/Controllers/CurrenciesController.php
@@ -3,11 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PHPHtmlParser\Dom;
 
 class CurrenciesController extends Controller
 {
+    private function getCurrenciesCurrentRate(){
+        $dom = new Dom;
+        $dom->load('http://webrates.truefx.com/rates/connect.html?f=html');
+        $rows=$dom->find('tr');
+        $array=array();
+        foreach ($rows as $row){
+            $stringValue=($row->find('td')[2]->text.$row->find('td')[3]->text);
+            array_push($array,array($row->find('td')[0]->text,floatval($stringValue)));
+        }
+        return $array;
+    }
+    private function getRandomColorForCurrencyDiv($currencyCount){
+        $currency=array();
+        for ($i=0;$i<$currencyCount;$i++){
+            $randNum=rand(0,5);
+            switch($randNum){
+                case 0: array_push($currency,'green');
+                    break;
+                case 1: array_push($currency,'yellow');
+                    break;
+                case 2: array_push($currency,'orange');
+                    break;
+                case 3: array_push($currency,'blue');
+                    break;
+                case 4: array_push($currency,'red');
+                    break;
+                case 5: array_push($currency,'purple');
+            }
+        }
+        return $currency;
+    }
+
     public function getAll(){
-        return view('dashboard');
+        $currencies=$this->getCurrenciesCurrentRate();
+        $colors=$this->getRandomColorForCurrencyDiv(count($currencies));
+        return view('dashboard', compact('currencies','colors'));
+
     }
     public function get($id){
         return view('buy');
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,66 +1,19 @@
 @extends('master')
 @section('content')
+    @foreach($currencies as $currency)
+        @if($loop->index % 4 ==0)
             <div class="row-fluid">
-
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
+        @endif
+                <div class="statbox {{$colors[$loop->index]}} span6" ontablet="span6" ondesktop="span3">
+                    <div class="number">{{$currency[0]}}</div>
+                    <div class="title">Wartość: {{$currency[1]}}</div>
                     <div class="footer">
                         <a href="#">Inwestuj</a>
                     </div>
                 </div>
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="row-fluid">
-                <div class="statbox orange span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Obecny kurs: 4.85</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-                <div class="statbox red span6" ontablet="span6" ondesktop="span3">
-                    <div class="number">EUR/USD</div>
-                    <div class="title">Wartość: 4.85<br>Stopa zwrotu: 80%</div>
-                    <div class="footer">
-                        <a href="#">Inwestuj</a>
-                    </div>
-                </div>
-            </div>
+        @if($loop->index % 4 ==3)
+           </div>
+        @endif
+    @endforeach
 @endsection
 
